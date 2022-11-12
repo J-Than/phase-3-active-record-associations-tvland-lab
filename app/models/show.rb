404ff17ddd02b5767679a96fd360e11a689567da
Jarod Wilson <jaroddwilson@gmail.com>
2022-11-12 18:24:56 -0500

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    ret = []
+    self.actors.each do |a|
+      ret.push "#{a.first_name} #{a.last_name}"
+    end
+    ret
+  end
+
 end
