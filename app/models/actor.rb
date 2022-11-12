3e12c19b2c680b860ac535feae141230e30db34a
Jarod Wilson <jaroddwilson@gmail.com>
2022-11-12 18:24:41 -0500

    commit tree 178b11ffe1d59c9caa09e8287c50f43cf117955e

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    ret = []
+    self.characters.each do |c|
+      ret.push "#{c.name} - #{c.show.name}"
+    end
+    ret
+  end
+
 end
